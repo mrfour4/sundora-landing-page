f249ece09ae1fc073670d8089917a9ee6be76e13
mrfour4 <21521609@gm.uit.edu.vn>
2025-08-05 02:30:43 +0700

    refactor: library page

--- src/modules/library.tsx
+++ src/modules/library.tsx
@@ -14,18 +14,18 @@ export const Library = () => {
     return (
         <section
             id={ESectionId.LIBRARY}
-            className="bg-accent-foreground flex h-screen items-center gap-40 pt-10"
+            className="bg-accent-foreground flex h-screen items-center gap-x-32 pt-10"
         >
             <div>
                 <Image
                     src="/images_avif/library-1.avif"
                     alt="Hình ảnh dự án"
-                    width={722}
-                    height={696}
+                    width={385}
+                    height={371}
                     className="shrink-0 object-cover"
                 />
-                <div className="from-secondary-foreground to-secondary flex h-24 items-center bg-gradient-to-b pl-16">
-                    <p className="text-primary text-xl font-semibold">
+                <div className="from-secondary-foreground to-secondary flex h-14 items-center bg-gradient-to-b pl-8">
+                    <p className="text-primary text-sm font-semibold">
                         Hình ảnh dự án
                     </p>
                 </div>
@@ -34,7 +34,7 @@ export const Library = () => {
                 <div className="flex items-center gap-8">
                     <h1
                         className={cn(
-                            "text-7xl font-light text-white uppercase",
+                            "text-5xl font-light text-white uppercase",
                             albra.className,
                         )}
                     >
@@ -42,25 +42,25 @@ export const Library = () => {
                     </h1>
                     <Button
                         variant="ghost"
-                        className="border-secondary hover:text-secondary cursor-pointer rounded-full border-2 text-xl font-medium text-white hover:bg-transparent!"
+                        className="border-secondary hover:text-secondary cursor-pointer rounded-full border-2 text-sm font-medium text-white hover:bg-transparent!"
                     >
                         <Icons.isolation className="shrink-0" />
                         <span>VR 360 DỰ ÁN</span>
                         <Icons.arrowLeft className="text-secondary shrink-0 rotate-180" />
                     </Button>
                 </div>
-                <div className="mt-20 flex gap-x-24">
+                <div className="mt-10 flex gap-x-24">
                     <div>
                         <Link href="#!" className="relative">
                             <Image
                                 src="/images_avif/library-2.avif"
                                 alt="Video dự án"
-                                width={420}
-                                height={305}
+                                width={336}
+                                height={243}
                             />
-                            <Icons.play className="absolute top-1/2 left-1/2 size-20 -translate-1/2" />
+                            <Icons.play className="absolute top-1/2 left-1/2 size-12 -translate-1/2" />
                         </Link>
-                        <p className="mt-7 text-xl font-medium text-white">
+                        <p className="mt-4 text-sm font-medium text-white">
                             Video dự án
                         </p>
                     </div>
@@ -69,11 +69,11 @@ export const Library = () => {
                             <Image
                                 src="/images_avif/library-3.avif"
                                 alt="Tài liệu & CSBH"
-                                width={420}
-                                height={305}
+                                width={336}
+                                height={243}
                             />
                         </Link>
-                        <p className="mt-7 text-xl font-medium text-white">
+                        <p className="mt-4 text-sm font-medium text-white">
                             Tài liệu & CSBH
                         </p>
                     </div>
