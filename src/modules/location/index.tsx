b8c6ff1d85f4f0b7c84f2a356c8be53f1fefa86b
mrfour4 <21521609@gm.uit.edu.vn>
2025-08-05 00:11:28 +0700

    refactor: location page

--- src/modules/location/index.tsx
+++ src/modules/location/index.tsx
@@ -12,37 +12,35 @@ export const Location = () => {
     return (
         <section
             id={ESectionId.LOCATION}
-            className="h-screen bg-cover bg-no-repeat px-40 pt-40"
+            className="flex h-screen flex-col justify-center bg-cover bg-no-repeat px-24 pt-20"
             style={{ backgroundImage: `url(${LOCATION_IMG})` }}
         >
             <div
                 className={cn(
-                    "text-shadow-secondary text-7xl font-light text-white uppercase text-shadow-[0_0_10px]",
+                    "text-shadow-secondary text-5xl font-light text-white uppercase text-shadow-[0_0_10px]",
                     albra.className,
                 )}
             >
                 <h1>vị trí trung tâm</h1>
-                <h2 className="mt-1 ml-36">của trung tâm</h2>
+                <h2 className="ml-20">của trung tâm</h2>
             </div>
             <h2
                 className={cn(
-                    "from-secondary to-secondary-foreground -mt-3 bg-gradient-to-b bg-clip-text text-4xl leading-20 text-transparent",
+                    "from-secondary to-secondary-foreground -mt-3 bg-gradient-to-b bg-clip-text text-xl leading-16 text-transparent",
                     sparkling.className,
                 )}
             >
                 Hiếm hoi còn lại bên Sông Hàn
             </h2>
 
-            <div className="mt-2 w-1/2 space-y-5 text-justify text-lg text-white">
+            <div className="w-1/2 text-sm text-white">
                 <p>
                     Giữa lòng Đà Nẵng nơi quỹ đất ven sông ngày càng trở nên
                     khan hiếm, dự án sở hữu vị trí vàng, gắn liền với bản sắc và
-                    sự phát triển thịnh vượng của thành phố.
-                </p>
-                <p>
-                    Một giao điểm của các trục kết nối hành chính thương mại du
-                    lịch trọng yếu, một tọa độ kim cương có khả năng sinh lời
-                    bền vững theo thời gian.
+                    sự phát triển thịnh vượng của thành phố. Một giao điểm của
+                    các trục kết nối hành chính thương mại du lịch trọng yếu,
+                    một tọa độ kim cương có khả năng sinh lời bền vững theo thời
+                    gian.
                 </p>
             </div>
 
--- src/modules/location/travel-timelines.tsx
+++ src/modules/location/travel-timelines.tsx
@@ -4,14 +4,14 @@ import { cn } from "@/lib/utils";
 
 export const TravelTimeline = () => {
     return (
-        <div className="mt-8">
+        <div className="mt-3">
             {TRAVEL_NEARBY_DATA.map((group) => (
                 <div key={group.duration}>
                     <div className="relative pl-3">
                         <div className="bg-secondary-foreground absolute top-1/2 left-0 size-2.5 -translate-x-1/2 -translate-y-1/2 rounded-full" />
                         <h3
                             className={cn(
-                                "text-shadow-secondary text-2xl font-light text-white uppercase text-shadow-[0_0_10px]",
+                                "text-shadow-secondary text-sm font-light text-white uppercase text-shadow-[0_0_10px]",
                                 albra.className,
                             )}
                         >
@@ -20,7 +20,7 @@ export const TravelTimeline = () => {
                     </div>
                     <ul
                         className={cn(
-                            "text-secondary border-secondary-foreground relative grid w-lg grid-cols-2 gap-x-4 gap-y-0.5 border-l border-dashed pt-2 pb-4 pl-3 text-[10px] leading-3.5",
+                            "text-secondary border-secondary-foreground relative grid w-[400px] grid-cols-2 gap-x-4 border-l border-dashed pb-2 pl-3 text-[8px] leading-3.5",
                             group.locations.length < 5 && "grid-cols-1",
                         )}
                     >
