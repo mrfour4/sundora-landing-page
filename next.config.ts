897f3446dacc3a0b4cdb01d1f7b64b30f85bd007
mrfour4 <21521609@gm.uit.edu.vn>
2025-08-27 02:01:52 +0700

    fix: next config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const UPLOADTHING = `${process.env.UPLOADTHING_HOST!}/f/**`;
+const UPLOADTHING = "https://2xdfpc0xqz.ufs.sh/f/**";
 
 const nextConfig: NextConfig = {
     images: {
